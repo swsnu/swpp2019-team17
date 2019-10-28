bb4b88c2839238c26ba2621ae583672ead20b967
ske-kr <ske2604@gmail.com>
2019-10-29 00:57:08 +0900

    mysql, tutoring_app created

--- backend/tutoring/urls.py
+++ backend/tutoring/urls.py
@@ -5,5 +5,14 @@ urlpatterns = [
     path('signup/tutee_manager', views.signup_tutee_manager, name='signup'),
     path('signup/tutor', views.signup_tutor, name='signup'),
     path('signin', views.signin, name='signin'),
-    path('signout', views.signout, name='signin'),
+    path('signout', views.signout, name='signout'),
+
+    path('tutor/<int:tutor_id>',views.tutor_page_profile,name='tutor_profile'),
+    path('tutor/review/<int:tutor_id>',views.tutor_page_review,name='tutor_review'),
+    path('tutor/tutoring/<int:tutor_id>',views.tutor_page_tutoring,name='tutor_tutoring'),
+    
+    path('tutee',views.tutee_page_create,name='tutee_profile'),
+    path('tutee/<int:tutor_id>',views.tutee_page_profile,name='tutee_profile'),
+    path('tutee/review/<int:tutor_id>',views.tutee_page_review,name='tutee_profile'),
+    path('tutee/tutoring/<int:tutor_id>',views.tutee_page_tutoring,name='tutee_profile'),
 ]
--- backend/tutoring/views.py
+++ backend/tutoring/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 import json
 # Create your views here.
 
+
+
 def signin(request):
     if request.method == 'POST':
         return HttpResponse(status=401)
@@ -26,3 +28,51 @@ def signup_tutor(request):
         return HttpResponse(status=201)
     else:
         return HttpResponse(status=405)
+
+'''
+tutors page
+'''
+def tutor_page_review(request,tutor_id):
+    '''
+    implement
+    '''
+    return HttpResponse(status=404)
+
+def tutor_page_profile(request,tutor_id):
+    '''
+    implement
+    '''
+    return HttpResponse(status=404)   
+
+def tutor_page_tutoring(request,tutor_id):
+    '''
+    implement
+    '''
+    return HttpResponse(status=404)   
+
+'''
+tutee_managers page
+'''
+def tutee_page_create(request,tutor_id):
+    '''
+    implement
+    '''
+    return HttpResponse(status=404)  
+
+def tutee_page_profile(request,tutor_id):
+    '''
+    implement
+    '''
+    return HttpResponse(status=404)       
+
+def tutee_page_review(request,tutor_id):
+    '''
+    implement
+    '''
+    return HttpResponse(status=404)
+
+def tutee_page_tutoring(request,tutor_id):
+    '''
+    implement
+    '''
+    return HttpResponse(status=404)
