5c0eb83820fe776823948b487ff816d0588a79d6
ske-kr <ske2604@gmail.com>
2019-10-31 14:55:38 +0900

    model update_first

--- backend/tutoring/models.py
+++ backend/tutoring/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    
+    phonenumber=models.CharField(blank=False, max_length=13)
+    address=models.CharField(max_length=40)
+
+class Tutor(User):
+    '''
+    schedule imagefield or 
+    certificate imagefield
+    address
+    '''
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+    )
+    gender=models.CharField(max_length=20, choices=GENDER_CHOICES)
+    subject=models.CharField(max_length=40)
+
+class TuteeManager(User):
+    '''
+    tutee=onetomany field
+    '''
+    tutee_list=models.ManyToManyField("Tutee")
+
+class Tutee(models.Model):
+    '''
+    tutee=
+    '''
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+    )
+
+    tutee_manager=models.ForeignKey(
+        TuteeManager,
+        on_delete=models.CASCADE,
+    )
+    gender=models.CharField(max_length=20, choices=GENDER_CHOICES)
+    subject=models.CharField(max_length=40)
