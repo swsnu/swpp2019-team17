8ed1427f660f1609076fb2354c7583bb0aeec041
ske-kr <ske2604@gmail.com>
2019-11-01 15:32:28 +0900

    requirements, db, model.py update(not tested)
    will upload after testing all models signin&up

--- backend/tutoring/models.py
+++ backend/tutoring/models.py
@@ -43,3 +43,5 @@ class Tutee(models.Model):
     )
     gender=models.CharField(max_length=20, choices=GENDER_CHOICES)
     subject=models.CharField(max_length=40)
+    detailed_address_x=models.CharField(max_length=40)
+    detailed_address_y=models.CharField(max_length=40)
