06edc16e8176c374f0e2f6c078ec5f12e0a3e78a
qgusgh <hyunho1997@naver.com>
2019-11-04 16:44:38 +0900

    modified for eslint

--- frontend/src/components/SignUp/TuteeSignupForm.js
+++ frontend/src/components/SignUp/TuteeSignupForm.js
@@ -5,13 +5,14 @@ class TuteeSignupForm extends Component {
   render() {
     return (
       <div>
-        <label className="child-name-label">Name:</label>
+        <label className="child-name-label">Name
         <input
           className="child-name-input"
           onChange={e => this.props.onChangeName(e.target.value, this.props.id)}
           value={this.props.name}
         />
-        <button onClick={e => this.props.onClickDelete(this.props.id)}>delete</button>
+        </label>
+        <button onClick={() => this.props.onClickDelete(this.props.id)}>delete</button>
         {/* <label className="child-birthday-label">Birthday</label>
           <DatePicker
             onChange={this.setBirthday}
