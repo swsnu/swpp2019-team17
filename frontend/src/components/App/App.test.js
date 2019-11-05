436c78e94d344b5afc57061e15fc6a3a18badbca
ske-kr <ske2604@gmail.com>
2019-11-05 19:19:39 +0900

    final edit

--- frontend/src/components/App/App.test.js
+++ frontend/src/components/App/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from '../../redux/store';
+import { history } from '../../redux/reducer';
+import { Provider } from 'react-redux';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App history={history} />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
