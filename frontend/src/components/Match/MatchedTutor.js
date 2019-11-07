37b762f9fc2c3752461ba4a4806a6997f16e297a
qgusgh <hyunho1997@naver.com>
2019-11-07 14:47:30 +0900

    bottom border in tutorlist

--- frontend/src/components/Match/MatchedTutor.js
+++ frontend/src/components/Match/MatchedTutor.js
@@ -6,43 +6,42 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-
 function MatchedTutor(props) {
-    return (
-        <Media>
-            <img
-                width={64}
-                height={64}
-                src={props.profile}
-                alt="Profile photo"
-            />
-            <Media.Body>
-                <Container>
-                    <Row>
-                        <Col>
-                            <p><b>{props.name}</b></p>
-                            <p>{props.gender}</p>
-                        </Col>
-                        <Col>
-                                <p>{props.subject}</p>
-                        </Col>
-                        <Col>
-                            <p>{props.available[0]}</p>
-                            <p>{props.available[1]}</p>
-                        </Col>
-                    </Row>
-                </Container>
-            </Media.Body>
-        </Media>
+  return (
+    <Media className="border-bottom border-dark">
+      <img
+        width={64}
+        height={64}
+        src={props.profile}
+        alt="Profile photo"
+      />
+      <Media.Body>
+        <Container>
+          <Row>
+            <Col>
+              <p><b>{props.name}</b></p>
+              <p>{props.gender}</p>
+            </Col>
+            <Col>
+              <p>{props.subject}</p>
+            </Col>
+            <Col>
+              <p>{props.available[0]}</p>
+              <p>{props.available[1]}</p>
+            </Col>
+          </Row>
+        </Container>
+      </Media.Body>
+    </Media>
 
-    )
+  )
 }
 export default MatchedTutor;
 
 /*
                 <div className='matchedtutor'>
                     <div className="information">
-                        <p>{props.gender} | 
+                        <p>{props.gender} |
                         <button>Review</button> <button onClick={()=> {alert("Your request has been sent to the tutor")}}>Request</button>
                     </div>
                     <div className="available">
