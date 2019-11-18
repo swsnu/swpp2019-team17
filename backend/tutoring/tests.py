52af3998d79b739d4843312bee8c103e583d35e1
ske-kr <ske2604@gmail.com>
2019-11-18 12:27:35 +0900

    Update tests.py

--- backend/tutoring/tests.py
+++ backend/tutoring/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from .models import Tutee,TuteeManager,Tutor
 # Create your tests here.
+
+class ModelTest(TestCase):
+    def test_model(self):
+        test_tutor = Tutor.objects.create_user(username='test_id',password='test')
+
+class tokenTest(TestCase):
+    def test_csrf(self):
+        client = Client(enforce_csrf_checks=True)
+        response = client.post('/signup/tutor/', json.dumps({'username': 'chris', 'password': 'chris'}),
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 403)  # Request without csrf token returns 403 response
+
+        response = client.get('/token/')
+        self.assertEqual(response.status_code, 204)
+        csrftoken = response.cookies['csrftoken'].value  # Get csrf token from cookie
+
+        response = client.post('/signup/tutor/', json.dumps({'username': 'chris', 'password': 'chris'}),
+                               content_type='application/json', HTTP_X_CSRFTOKEN=csrftoken)
+        self.assertEqual(response.status_code, 201)  # Pass csrf protection
