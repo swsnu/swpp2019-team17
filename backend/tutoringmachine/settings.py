f8be3b8a410fa833e0f24255fe9cb360806eed1e
ske-kr <ske2604@gmail.com>
2019-12-17 17:13:15 +0900

    Update settings.py

--- backend/tutoringmachine/settings.py
+++ backend/tutoringmachine/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tutoringmachine_db',
-        'USER': 'tutoring',
-        'PASSWORD': 'machine',
+        'USER': 'root',
+        'PASSWORD': 'password',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
